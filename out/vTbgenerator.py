794a34d7b66aa64dd4c680ebcbb463dd540950b1
ice-forever <74451255+ice-forever@users.noreply.github.com>
2022-09-03 12:54:25 +0800

    增加了保存tb文件的功能

--- out/vTbgenerator.py
+++ out/vTbgenerator.py
@@ -23,39 +23,61 @@ Usage:
       python vTbgenerator.py ModuleFileName.v
 
 '''
-
+import chardet
 import re
 import sys
-import chardet
 
-def delComment( Text ):
+
+class Logger(object):
+    def __init__(self, filename='default.log', stream=sys.stdout):
+        self.terminal = stream
+        self.log = open(filename, 'w')
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)
+
+    def flush(self):
+        pass
+
+
+# 将控制台的结果输出到a.log文件，可以改成a.txt
+sys.stdout = Logger('tb_'+sys.argv[1].split('\\')[-1], sys.stdout)
+#sys.stderr = Logger('a.log_file', sys.stderr)
+
+
+def delComment(Text):
     """ removed comment """
     single_line_comment = re.compile(r"//(.*)$", re.MULTILINE)
-    multi_line_comment  = re.compile(r"/\*(.*?)\*/",re.DOTALL)
-    Text = multi_line_comment.sub('\n',Text)
-    Text = single_line_comment.sub('\n',Text)
+    multi_line_comment = re.compile(r"/\*(.*?)\*/", re.DOTALL)
+    Text = multi_line_comment.sub('\n', Text)
+    Text = single_line_comment.sub('\n', Text)
     return Text
 
-def delBlock( Text ) :
+
+def delBlock(Text):
     """ removed task and function block """
-    Text = re.sub(r'\Wtask\W[\W\w]*?\Wendtask\W','\n',Text)
-    Text = re.sub(r'\Wfunction\W[\W\w]*?\Wendfunction\W','\n',Text)
+    Text = re.sub(r'\Wtask\W[\W\w]*?\Wendtask\W', '\n', Text)
+    Text = re.sub(r'\Wfunction\W[\W\w]*?\Wendfunction\W', '\n', Text)
     return Text
+
 
 def findName(inText):
     """ find module name and port list"""
-    p = re.search(r'([a-zA-Z_][a-zA-Z_0-9]*)\s*',inText)
+    p = re.search(r'([a-zA-Z_][a-zA-Z_0-9]*)\s*', inText)
     mo_Name = p.group(0).strip()
     return mo_Name
 
-def paraDeclare(inText ,portArr) :
+
+def paraDeclare(inText, portArr):
     """ find parameter declare """
-    pat = r'\s'+ portArr + r'\s[\w\W]*?[;,)]'
-    ParaList = re.findall(pat ,inText)
+    pat = r'\s' + portArr + r'\s[\w\W]*?[;,)]'
+    ParaList = re.findall(pat, inText)
 
     return ParaList
 
-def portDeclare(inText ,portArr) :
+
+def portDeclare(inText, portArr):
     """find port declare, Syntax:
        input [ net_type ] [ signed ] [ range ] list_of_port_identifiers
 
@@ -67,114 +89,119 @@ def portDeclare(inText ,portArr) :
         (?P<port_list>.*?)
         (?= \binput\b | \boutput\b | \binout\b | ; | \) )
         ''',
-        re.VERBOSE|re.MULTILINE|re.DOTALL
+        re.VERBOSE | re.MULTILINE | re.DOTALL
     )
 
     pList = port_definition.findall(inText)
 
     t = []
     for ls in pList:
-        if len(ls) >=2  :
-            t = t+ portDic(ls[-2:])
+        if len(ls) >= 2:
+            t = t + portDic(ls[-2:])
     return t
 
-def portDic(port) :
+
+def portDic(port):
     """delet as : input a =c &d;
         return list as : (port, [range])
     """
     pRe = re.compile(r'(.*?)\s*=.*', re.DOTALL)
 
     pRange = port[0]
-    pList  = port[1].split(',')
-    pList  = [ i.strip() for i in pList if i.strip() !='' ]
-    pList  = [(pRe.sub(r'\1', p), pRange.strip() ) for p in pList ]
+    pList = port[1].split(',')
+    pList = [i.strip() for i in pList if i.strip() != '']
+    pList = [(pRe.sub(r'\1', p), pRange.strip()) for p in pList]
 
     return pList
 
-def formatPort(AllPortList,isPortRange =1) :
+
+def formatPort(AllPortList, isPortRange=1):
     PortList = AllPortList[0] + AllPortList[1] + AllPortList[2]
 
-    str =''
-    if PortList !=[] :
+    str = ''
+    if PortList != []:
         l1 = max([len(i[0]) for i in PortList])+2
         l2 = max([len(i[1]) for i in PortList])
         l3 = max(24, l1)
 
         strList = []
-        for pl in AllPortList :
-            if pl  != [] :
-                str = ',\n'.join( [' '*4+'.'+ i[0].ljust(l3)
-                                  + '( '+ (i[0].ljust(l1 )+i[1].ljust(l2))
-                                  + ' )' for i in pl ] )
-                strList = strList + [ str ]
+        for pl in AllPortList:
+            if pl != []:
+                str = ',\n'.join([' '*4+'.' + i[0].ljust(l3)
+                                  + '( ' + (i[0].ljust(l1)+i[1].ljust(l2))
+                                  + ' )' for i in pl])
+                strList = strList + [str]
 
         str = ',\n\n'.join(strList)
 
     return str
 
-def formatDeclare(PortList,portArr, initial = "" ):
-    str =''
-    if initial !="" :
+
+def formatDeclare(PortList, portArr, initial=""):
+    str = ''
+    if initial != "":
         initial = " = " + initial
 
-    if PortList!=[] :
-        str = '\n'.join( [ portArr.ljust(4) +'  '+(i[1]+min(len(i[1]),1)*'  '
-                           +i[0]).ljust(36)+ initial + ' ;' for i in PortList])
+    if PortList != []:
+        str = '\n'.join([portArr.ljust(4) + '  '+(i[1]+min(len(i[1]), 1)*'  '
+                                                  + i[0]).ljust(36) + initial + ' ;' for i in PortList])
     return str
 
-def formatPara(ParaList) :
+
+def formatPara(ParaList):
     paraDec = ''
     paraDef = ''
-    if ParaList !=[]:
-        s = '\n'.join( ParaList)
+    if ParaList != []:
+        s = '\n'.join(ParaList)
         pat = r'([a-zA-Z_][a-zA-Z_0-9]*)\s*=\s*([\w\W]*?)\s*[;,)]'
-        p = re.findall(pat,s)
+        p = re.findall(pat, s)
 
-        l1 = max([len(i[0] ) for i in p])
-        l2 = max([len(i[1] ) for i in p])
-        paraDec = '\n'.join( ['parameter %s = %s;'
-                             %(i[0].ljust(l1 +1),i[1].ljust(l2 ))
+        l1 = max([len(i[0]) for i in p])
+        l2 = max([len(i[1]) for i in p])
+        paraDec = '\n'.join(['parameter %s = %s;'
+                             % (i[0].ljust(l1 + 1), i[1].ljust(l2))
                              for i in p])
-        paraDef =  '#(\n' +',\n'.join( ['    .'+ i[0].ljust(l1 +1)
-                    + '( '+ i[0].ljust(l1 )+' )' for i in p])+ ')\n'
+        paraDef = '#(\n' + ',\n'.join(['    .' + i[0].ljust(l1 + 1)
+                                       + '( ' + i[0].ljust(l1)+' )' for i in p]) + ')\n'
     else:
         l1 = 6
         l2 = 2
-    preDec = '\n'.join( ['parameter %s = %s;\n'
-                             %('PERIOD'.ljust(l1 +1), '10'.ljust(l2 ))])
+    preDec = '\n'.join(['parameter %s = %s;\n'
+                        % ('PERIOD'.ljust(l1 + 1), '10'.ljust(l2))])
     paraDec = preDec + paraDec
-    return paraDec,paraDef
+    return paraDec, paraDef
+
 
 def writeTestBench(input_file):
     """ write testbench to file """
     with open(input_file, 'rb') as f:
-        f_info =  chardet.detect(f.read())
+        f_info = chardet.detect(f.read())
         f_encoding = f_info['encoding']
     with open(input_file, encoding=f_encoding) as inFile:
-        inText  = inFile.read()
+        inText = inFile.read()
 
     # removed comment,task,function
     inText = delComment(inText)
-    inText = delBlock  (inText)
+    inText = delBlock(inText)
 
     # moduel ... endmodule  #
-    moPos_begin = re.search(r'(\b|^)module\b', inText ).end()
-    moPos_end   = re.search(r'\bendmodule\b', inText ).start()
+    moPos_begin = re.search(r'(\b|^)module\b', inText).end()
+    moPos_end = re.search(r'\bendmodule\b', inText).start()
     inText = inText[moPos_begin:moPos_end]
 
-    name  = findName(inText)
-    paraList = paraDeclare(inText,'parameter')
-    paraDec , paraDef = formatPara(paraList)
+    name = findName(inText)
+    paraList = paraDeclare(inText, 'parameter')
+    paraDec, paraDef = formatPara(paraList)
 
-    ioPadAttr = [ 'input','output','inout']
-    input  =  portDeclare(inText,ioPadAttr[0])
-    output =  portDeclare(inText,ioPadAttr[1])
-    inout  =  portDeclare(inText,ioPadAttr[2])
+    ioPadAttr = ['input', 'output', 'inout']
+    input = portDeclare(inText, ioPadAttr[0])
+    output = portDeclare(inText, ioPadAttr[1])
+    inout = portDeclare(inText, ioPadAttr[2])
 
-    portList = formatPort( [input , output , inout] )
-    input  = formatDeclare(input ,'reg', '0' )
-    output = formatDeclare(output ,'wire')
-    inout  = formatDeclare(inout ,'wire')
+    portList = formatPort([input, output, inout])
+    input = formatDeclare(input, 'reg', '0')
+    output = formatDeclare(output, 'wire')
+    inout = formatDeclare(inout, 'wire')
 
     # write testbench
     timescale = '`timescale  1ns / 1ps\n'
@@ -183,14 +210,14 @@ def writeTestBench(input_file):
     print("module tb_%s;\n" % name)
 
     # module_parameter_port_list
-    if(paraDec!=''):
+    if (paraDec != ''):
         print("// %s Parameters\n%s\n" % (name, paraDec))
 
     # list_of_port_declarations
-    print("// %s Inputs\n%s\n"  % (name, input ))
+    print("// %s Inputs\n%s\n" % (name, input))
     print("// %s Outputs\n%s\n" % (name, output))
-    if(inout!=''):
-        print("// %s Bidirs\n%s\n"  % (name, inout ))
+    if (inout != ''):
+        print("// %s Bidirs\n%s\n" % (name, inout))
 
     # print clock
     clk = '''
@@ -199,11 +226,11 @@ begin
     forever #(PERIOD/2)  clk=~clk;
 end'''
     rst = " "
-    
-    print("%s\n%s" % (clk,rst))
+
+    print("%s\n%s" % (clk, rst))
 
     # UUT
-    print("%s %s u_%s (\n%s\n);" %(name,paraDef,name,portList))
+    print("%s %s u_%s (\n%s\n);" % (name, paraDef, name, portList))
 
     # print operation
     operation = '''
@@ -215,6 +242,7 @@ end
 '''
     print(operation)
     print("endmodule")
+
 
 if __name__ == '__main__':
     writeTestBench(sys.argv[1])
